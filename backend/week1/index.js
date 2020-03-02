2a8cc78097b5e41ae807f7943ba74fa9775ffe53
saralof <60660486+saralof@users.noreply.github.com>
2020-03-02 14:48:19 +0100

    updateu
    update

--- backend/week1/index.js
+++ backend/week1/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
 
+app.use(express.static('static'));
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+/*
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/about', (req, res) => {
@@ -10,10 +18,15 @@ app.get('/about', (req, res) => {
 
 app.get('/contact', (req, res) => {
     res.send('There is contact information')
-});
+}); */
 
 app.get('*', (req, res) => {
     res.send('Error 404: Page Not Found')
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path');
+
+app.use(express.static('static'));
+
+/*app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => res.render('pages/index'));
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/about', (req, res) => {
+    res.send('this is about')
+});
+
+app.get('/contact', (req, res) => {
+    res.send('There is contact information')
+}); */
+
+app.get('*', (req, res) => {
+    res.send('Error 404: Page Not Found')
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+
+
