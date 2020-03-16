cd3833a848ca2aedceaacb3c527bd88032ea648f
saralof <60660486+saralof@users.noreply.github.com>
2020-03-16 13:37:43 +0100

    users added
    users are added

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const port = 3000;
 const path = require('path');
 const ejs = require('ejs');
 const fs = require('fs');
-const bodyParser = require ('body-parser')
-const slug = require('slug')
+const bodyParser = require ('body-parser');
+const slug = require('slug');
 const myCss = {
     style : fs.readFileSync('static/css/styles.css','utf8')
 };
 const jquery = require('jquery');
+const session = require('express-session'); 
+const cookieParser = require('cookie-parser');
 
 //database set-up
-const mongodb = require('mongodb')
+const mongodb = require('mongodb');
 const mongoose = require('mongoose');
 mongoose
 .connect('mongodb://localhost:27017/datingapp', {
@@ -31,15 +33,25 @@ let chatSchema = new mongoose.Schema({
 });
  
 let berichten = mongoose.model('berichten', chatSchema);
-app.get('/chatwindow', (req, res) =>
-berichten.find(function(err, messages){
-    if (err) {
-        console.log(err);
-    }else{
-        res.render('chatwindow', {myCss: myCss, messages: messages})
-        console.log(messages);
-    }
-}));
+
+let users = [{
+    name: 'Jonny',
+    age: 25,
+    description: 'I am Jonny. I am looking for someone to take out into nature and have picknicks with.'
+},
+{
+    name: 'Tonya',
+    age: 19,
+    description: 'I am Tonya Potter, I am new to dating and hope to explore it in a non-pressuring way.'
+
+},
+{
+    name:'Codie',
+    age: 29,
+    description: 'My name is Codie. I am looking for someone to bond with for the long haul.'
+}];
+
+
 
 //express set-up
 app.use(express.static('static'));
@@ -47,9 +59,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-app.get('/chatoverview', (req, res) => res.render('chatoverview', {myCss: myCss}));
-//app.get('/chatwindow', (req, res) => res.render('chatwindow', {myCss: myCss, chatSchema: chatSchema, mytext: 'hello'}));
-
+app.get('/chatoverview', (req, res) => 
+berichten.find(function(err, messages){
+    if (err) {
+        console.log(err);
+    }else{
+        res.render('chatoverview', {myCss: myCss, users: users, messages: messages})
+    }
+}));
+app.get('/chatwindow', (req, res) =>
+berichten.find(function(err, messages){
+    if (err) {
+        console.log(err);
+    }else{
+        res.render('chatwindow', {myCss: myCss, users: users, messages: messages})
+        console.log(messages);
+    }
+}));
 app.get('/', (req, res) => res.render('index', {myCss: myCss}));
 app.get('*', (req, res) => {res.send('Error 404: Page Not Found')});
 app.post('/chatwindow', (req, res) =>{verstuur(req.body.message); res.redirect(req.originalUrl);});
@@ -63,7 +89,7 @@ function verstuur(msg){
             console.log('error during record insertion: ' + err);
         }
     });
-} 
+};
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
