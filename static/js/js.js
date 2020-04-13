ecdbdc8fdda1392f0d6606f14d7dbbfc1f3172d5
saralof <60660486+saralof@users.noreply.github.com>
2020-04-14 00:32:24 +0200

    docs
    docs

--- static/js/js.js
+++ static/js/js.js
@@ -1,11 +1,34 @@
+//Style changes depending on whether or not there's JS in browser
+const yesjs = document.getElementById("yesjs");
 
-let editMsg = document.querySelector('.i')
+yesjs.classList.remove("nojs");
+
+//Display edit field
+
+for (i = 0; i < 3; i++) {
+let x = i
+
+let editMsg = document.querySelector('#editButton' + x)
 
 function showBox() {
-    let clickEdit = document.getElementById(i+'_lbl')
-    let hideMsg = document.getElementById(i+'_txt')
-    clickEdit.classList.toggle('show')
-    hideMsg.classList.toggle('hide')
+    const clickEdit = document.getElementById('editMsg' + x)
+    const saveMsg = document.getElementById('saveMsg' + x)
+    const hideMsg = document.getElementById('theMsg' + x)
+    const hideButton = document.getElementById('editButton' + x)
+    clickEdit.classList.remove('hideEdit')
+    saveMsg.classList.remove('hideEdit')
+    hideMsg.classList.toggle('hideMsg')
+    hideButton.classList.toggle('hideButton')
 }
 
 editMsg.addEventListener("click", showBox)
+}
+
+
+function getRandomSub(){
+let subNr = Math.floor(Math.random() * 10)
+const randomSub = document.getElementById('sub' + subNr)
+randomSub.classList.remove('hideSub')
+}
+
+window.addEventListener('load', getRandomSub);
