4667d9ada3d75d278e432e92ce468609758975a8
saralof <60660486+saralof@users.noreply.github.com>
2020-04-14 10:58:42 +0200

    cleaning code
    cleaning code

--- static/js/js.js
+++ static/js/js.js
@@ -1,13 +1,15 @@
-//Style changes depending on whether or not there's JS in browser
+//Style changes depending on whether or not there's JS in browser, 
+//if there's no JS this won't load, so class "nojs" will remain on the body of the file
 const yesjs = document.getElementById("yesjs");
 
 yesjs.classList.remove("nojs");
 
-//Display edit field
+//Display edit field, the editfield is hidden behind a button (unless there's nojs), when the user clicks on this button the edit
+//field and save button will show up, while the edit button, and message will hide.
+//There's multiple messages, x will determine which one is being clicked and which one will be hidden/showed.
 
 for (i = 0; i < 3; i++) {
 let x = i
-
 let editMsg = document.querySelector('#editButton' + x)
 
 function showBox() {
@@ -24,7 +26,7 @@ function showBox() {
 editMsg.addEventListener("click", showBox)
 }
 
-
+//get one of the subs to remove the hidden css. User only sees this subject as suggestion to talk about
 function getRandomSub(){
 let subNr = Math.floor(Math.random() * 10)
 const randomSub = document.getElementById('sub' + subNr)
